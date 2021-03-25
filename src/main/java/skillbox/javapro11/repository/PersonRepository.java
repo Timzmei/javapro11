0f5ff21d400908ce28e72390dcb4e48d224659f5
Sergey <programmistsvs@gmail.com>
2021-03-25 23:02:25 +0700

    Spring security

---
+++ src/main/java/skillbox/javapro11/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package skillbox.javapro11.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import skillbox.javapro11.model.entity.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    Person findByEmail(String email);
+
+    Person findByEmailAndPassword(String email, String password);
+}
