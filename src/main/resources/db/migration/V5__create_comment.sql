b29cec3232708764fa0f98675d3969e11cc2b848
timur_guliev <timur.guliev@gmail.com>
2021-03-25 17:42:36 +0500

    Merge remote-tracking branch 'origin/Task_7' into Task_7

--- src/main/resources/db/migration/V5__create_comment.sql
+++ src/main/resources/db/migration/V5__create_comment.sql
@@ -1,4 +1,1 @@
 create table comment (id  bigserial not null, author_id int8, comment_text text, is_blocked boolean not null, parent_id int, time int8 not null, post_id int8 not null, primary key (id));
-alter table if exists comment add constraint FKs1slvnkuemjsq2kj4h3vhx7i1 foreign key (post_id) references post;
-
-
