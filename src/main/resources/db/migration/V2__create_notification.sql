b74d83e9336124802e7ad25e0e2546e2c59d5e8a
Ego-S <sizenko.egor@gmail.com>
2021-03-26 14:56:00 +0200

    change sent_time fild type from time to timestamp in migration for notification

--- src/main/resources/db/migration/V2__create_notification.sql
+++ src/main/resources/db/migration/V2__create_notification.sql
@@ -2,7 +2,7 @@ CREATE TABLE notification
 (
     id bigint NOT NULL,
     type_id bigint NOT NULL,
-    sent_time time NOT NULL,
+    sent_time timestamp NOT NULL,
     entity_id bigint NOT NULL,
     info varchar(255) NOT NULL,
     person_id bigint NOT NULL,
