c056f2766a7da506e5b20a181c15b93f5ce0554a
timur_guliev <timur.guliev@gmail.com>
2021-03-29 20:44:10 +0500

    Merge remote-tracking branch 'origin/Task_7_Entity_Post' into Task_7_Entity_Post

--- src/main/resources/db/migration/V3__create_post.sql
+++ src/main/resources/db/migration/V3__create_post.sql
@@ -1,1 +1,8 @@
-create table post (id  bigserial not null, is_blocked boolean not null, post_text text, time timestamp not null, title varchar(255), author_id int8 not null, primary key (id));
+create table post (
+    id  bigserial not null,
+    is_blocked boolean not null,
+    post_text text,
+    time timestamp not null,
+    title varchar(255),
+    author_id int8 not null,
+    primary key (id));
--- src/main/resources/db/migration/V5__create_comment.sql
+++ src/main/resources/db/migration/V5__create_comment.sql
@@ -1,1 +1,9 @@
-create table comment (id  bigserial not null, author_id int8, comment_text text, is_blocked boolean not null, parent_id int, time timestamp not null, post_id int8 not null, primary key (id));
+create table comment (
+    id  bigserial not null,
+    author_id int8,
+    comment_text text,
+    is_blocked boolean not null,
+    parent_id int,
+    time timestamp not null,
+    post_id int8 not null,
+    primary key (id));
