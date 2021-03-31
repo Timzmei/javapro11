fa733f3492e5c27671d79293b70f73367afc8bf7
timur_guliev <timur.guliev@gmail.com>
2021-04-01 01:08:50 +0500

    Merge remote-tracking branch 'origin/Task_7_Entity_Post' into Task_7_Entity_Post

---
+++ src/main/java/skillbox/javapro11/controller/StorageController.java
@@ -0,0 +1,19 @@
+package skillbox.javapro11.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import skillbox.javapro11.api.request.RegisterRequest;
+
+/**
+ * Created by timur_guliev on 31.03.2021.
+ */
+@RestController
+public class StorageController {
+
+    @PostMapping("/api/v1/storage")
+    public ResponseEntity storageFile(){
+        return null;
+    }
+}
