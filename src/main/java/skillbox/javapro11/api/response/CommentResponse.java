013523ea4492e811358ef5f6fbdac4e490a64951
AlexPan <panina.Alexa5@gmail.com>
2021-04-23 18:12:34 +0300

    edited mapper

--- src/main/java/skillbox/javapro11/api/response/CommentResponse.java
+++ src/main/java/skillbox/javapro11/api/response/CommentResponse.java
@@ -2,10 +2,7 @@ package skillbox.javapro11.api.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import skillbox.javapro11.model.entity.Comment;
 
 import java.time.LocalDateTime;
@@ -13,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/skillbox/javapro11/config/JavaproConfig.java
+++ src/main/java/skillbox/javapro11/config/JavaproConfig.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import skillbox.javapro11.api.response.CommentResponse;
+import skillbox.javapro11.model.entity.Comment;
 
 import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
@@ -17,6 +19,7 @@ public class JavaproConfig {
         .setFieldMatchingEnabled(true)
         .setSkipNullEnabled(true)
         .setFieldAccessLevel(PRIVATE);
+    mapper.createTypeMap(Comment.class, CommentResponse.class).addMappings(m -> m.map(com -> com.getPost().getId(), CommentResponse::setPostId));
     return mapper;
   }
 }
