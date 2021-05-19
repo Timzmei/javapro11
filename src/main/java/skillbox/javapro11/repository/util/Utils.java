392432e47bf31ea8bbde4ed8ec3ac31e525b0cf6
AlexPan <panina.Alexa5@gmail.com>
2021-05-19 21:12:57 +0300

    fix

--- src/main/java/skillbox/javapro11/repository/util/Utils.java
+++ src/main/java/skillbox/javapro11/repository/util/Utils.java
@@ -14,7 +14,7 @@ public class Utils {
   public static Pageable getPageable(long offset, int itemPerPage, Sort sort) {
     itemPerPage = itemPerPage == 0 ? 1 : itemPerPage;
     int page = (int) (offset / itemPerPage);
-    if (sort.isEmpty()) {
+    if (sort == null || sort.isEmpty()) {
       return PageRequest.of(page, itemPerPage);
     }else{
       return PageRequest.of(page, itemPerPage, sort);
